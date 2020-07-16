f7eb0cf5c96672f5c631d83bd942fa66833c664f
andreipaciurca <paciurca.andrei@outlook.com>
2020-07-16 13:34:50 +0300

    first commit on backend

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World, cutie PI!'))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`Local: http://localhost:${port}`))
--- readme.txt
+++
@@ -1,3 +0,0 @@
-This is a test file!!!
-Test 2
-Backend test
