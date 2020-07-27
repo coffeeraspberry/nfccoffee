03f2de5259f83274f48392933f43403d91c2e266
andreipaciurca <paciurca.andrei@outlook.com>
2020-07-27 16:55:30 +0300

    added /logs route

--- application/routes.py
+++ application/routes.py
@@ -9,3 +9,7 @@ def home():
 @app.route("/users", methods=['GET'])
 def users():
     return json.dumps([u._asdict() for u in Users.query.all()]) 
+
+@app.route("/logs", methods=['GET'])
+def logs():
+    return json.dumps([v._asdict() for v in Dates.query.all()])
