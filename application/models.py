9aaab1a986c1449ebb5bee60de6f750348fbff71
andreipaciurca <paciurca.andrei@outlook.com>
2020-07-27 15:01:46 +0300

    also it's 'False' not 'flase'

--- application/models.py
+++ application/models.py
@@ -17,7 +17,7 @@ class Users(db.Model, DictSerializable):
     userRelationship = db.relationship("Dates") # test
     UserName = db.Column(db.String(80), unique=True, nullable=False, default='user')
     Email = db.Column(db.String(120), unique=True, nullable=False, default='email')
-    Counter = db.Column(db.Integer, nullable=flase, default=0)
+    Counter = db.Column(db.Integer, nullable=False, default=0)
     LastAccess = db.Column(db.DateTime, nullable=True, default=datetime.now())
 
     
@@ -25,5 +25,5 @@ class Users(db.Model, DictSerializable):
 class Dates(db.Model, DictSerializable):
     __tablename__ = 'Dates'
     DateID = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    UserID = db.Column(db.Integer, db.ForeignKey('Users.UserID')) # test
+    UserID = db.Column(db.Integer, db.ForeignKey('Users.UserID'))
     DateLog = db.Column(db.DateTime, nullable=False, default=datetime.now())
