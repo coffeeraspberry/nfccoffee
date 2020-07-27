01e7e97b974d621646ed22dfa2e4d0b4ac183809
andreipaciurca <paciurca.andrei@outlook.com>
2020-07-27 14:20:45 +0300

    reorganized version 1.0.4; i hope it works

---
+++ application/routes.py
@@ -0,0 +1,12 @@
+from flask import request, render_template, make_response
+from datetime import datetime
+from flask import current_app
+from .models import db, Users, Dates
+
+@app.route("/")
+def home():
+    return 'Debug text: Hello world!'
+
+@app.route("/users", methods=['GET'])
+def users():
+    return json.dumps([u._asdict() for u in Users.query.all()]) 
