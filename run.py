6869ce78df5fc3cbe96375158787355f9ea87eb1
uic82959 <andrei-alexandru.paciurca@continental-corporation.com>
2020-07-28 15:59:03 +0300

    modified:   run.py

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import subprocess
 import os
 
 os.system("pkill -9 flask")
+os.system("pkill -9 python3")
 #flask = Popen(['/usr/bin/python3' ,'-m', 'app'], stdin=PIPE, stderr=PIPE, stdout=PIPE)
 subprocess.call('python3 -m app &', shell=True)
 subprocess.call('python3 nfcread.py',shell=True)
