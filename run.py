061616c5e6cbf20b842e189e3c3d009adfc4adbb
uic82959 <andrei-alexandru.paciurca@continental-corporation.com>
2020-07-28 15:54:55 +0300

    new file:   nfcread.py 	modified:   run.py

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ import os
 
 os.system("pkill -9 flask")
 #flask = Popen(['/usr/bin/python3' ,'-m', 'app'], stdin=PIPE, stderr=PIPE, stdout=PIPE)
-subprocess.call('python3 -m app', shell=True)
-print("\nAre you lost baby girl? All good!\n")
+subprocess.call('python3 -m app &', shell=True)
+subprocess.call('python3 nfcread.py',shell=True)
