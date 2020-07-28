203e3bc08e8bb5936f7b2b5ba056e2f4894fc757
uic82959 <andrei-alexandru.paciurca@continental-corporation.com>
2020-07-28 15:17:07 +0300

    modified:   run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from subprocess import Popen
 
 #os.system("python3 "+ "app.py &" )
-flask = Popen(['python3 ', 'app.py &'])
+flask = Popen(['/usr/bin/python3','python3 ' ,'app.py &'])
 
 print("hello, its working, flask is running backgound...\n")
