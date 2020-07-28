4a7c39c8c9bf3a6ee5469d43fb009db76bca0476
uic82959 <andrei-alexandru.paciurca@continental-corporation.com>
2020-07-28 12:41:46 +0300

    added scriptBackend.sh; Testing bash shell scripting

---
+++ scriptBackend.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+#start Flask backend app
+#back='cd /backend' #first change dir to backend
+flask='export FLASK_APP=app.py FLASK_ENV=development' #Flask vars; TO DO in producton: remove 'FLASK_ENV=development'
+run ='flask run'
+sh -c $(back ; flask; run) &>/dev/null &
