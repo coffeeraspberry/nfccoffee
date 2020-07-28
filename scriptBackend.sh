b10859b68c3ed8d69cd33117eee2484919d02e94
uic82959 <andrei-alexandru.paciurca@continental-corporation.com>
2020-07-28 14:13:37 +0300

    Almost perfect from my point of view :)

--- scriptBackend.sh
+++ scriptBackend.sh
@@ -3,5 +3,8 @@
 #start Flask backend app
 flask='export FLASK_APP=app.py FLASK_ENV=development' #Flask vars; TO DO in producton: remove 'FLASK_ENV=development'
 frun='python3 -m flask run'
+
+eval 'pkill -9 -f flask'
+
 bash -c $( eval $flask; eval $frun ) &
 echo "Servers run on other bash shell ; \n"
