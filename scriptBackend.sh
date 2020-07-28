ba90a20908700c2055641726e45c80666bd97101
uic82959 <andrei-alexandru.paciurca@continental-corporation.com>
2020-07-28 13:57:31 +0300

    Redo of this file v11. I think this is the real deal :)

--- scriptBackend.sh
+++ scriptBackend.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 #start Flask backend app
-#back='cd /backend' #first change dir to backend
 flask='export FLASK_APP=app.py FLASK_ENV=development' #Flask vars; TO DO in producton: remove 'FLASK_ENV=development'
 frun='python3 -m flask run'
-eval $flask
-eval $frun
+bash -c $( eval $flask; eval $frun ) &
+echo "Servers run on other bash shell ;)"
