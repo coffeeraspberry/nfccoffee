768ff9446bca10a9ba7d58edba06de6ff5b6db83
uic82959 <andrei-alexandru.paciurca@continental-corporation.com>
2020-07-28 14:56:34 +0300

    modified:   app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from application import app
-import threading
+from threading import Thread
 
 if __name__=='__main__':
     #app.run(debug=True)
-     threading.Thread(target=app.run).start()
+     Thread(target=app.run).run()
      print("Flask runs on thread! Continue scripting baby girl ;)\n")
