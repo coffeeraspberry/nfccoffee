3a261b5383e63a86d2031687342fbb8a65cac046
uic82959 <andrei-alexandru.paciurca@continental-corporation.com>
2020-07-28 16:11:46 +0300

    modified:   run.py

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 import subprocess
 import os
 
-#os.system("pkill -9 flask")
-#os.system("pkill -9 python3")
+os.system("pkill -9 python3")
 
 subprocess.call('python3 -m app &', shell=True)
 subprocess.call('python3 nfcread.py',shell=True)
