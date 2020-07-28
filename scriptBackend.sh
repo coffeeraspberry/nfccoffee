a2f6956bad631fb0457cd100467922bc2e562799
uic82959 <andrei-alexandru.paciurca@continental-corporation.com>
2020-07-28 13:53:26 +0300

    Redo of this file v10. Almost done :)

--- scriptBackend.sh
+++ scriptBackend.sh
@@ -3,6 +3,6 @@
 #start Flask backend app
 #back='cd /backend' #first change dir to backend
 flask='export FLASK_APP=app.py FLASK_ENV=development' #Flask vars; TO DO in producton: remove 'FLASK_ENV=development'
-frun ='python3 -m flask run'
+frun='python3 -m flask run'
 eval $flask
 eval $frun
