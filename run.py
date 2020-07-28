ec5e2aba119cb68f11c6d51f9030bcd6a4c2b152
uic82959 <andrei-alexandru.paciurca@continental-corporation.com>
2020-07-28 15:43:37 +0300

    modified:   run.py

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ import os
 
 os.system("pkill -9 flask")
 #flask = Popen(['/usr/bin/python3' ,'-m', 'app'], stdin=PIPE, stderr=PIPE, stdout=PIPE)
-call('python3 -m app', shell=True)
+subprocess.call('python3 -m app', shell=True)
 print("\nAre you lost baby girl? All good!\n")
