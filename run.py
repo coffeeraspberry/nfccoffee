e39204a2dcab3ce6eebb8e02d1e94ded032d5c84
uic82959 <andrei-alexandru.paciurca@continental-corporation.com>
2020-07-28 16:15:39 +0300

    modified:   run.py 	modified:   scriptBackend.sh

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 import subprocess
 
-subprocess.call('pkill -9 python3', shell=True)
 subprocess.call('python3 -m app &', shell=True)
 subprocess.call('python3 nfcread.py',shell=True)
--- scriptBackend.sh
+++ scriptBackend.sh
@@ -4,6 +4,7 @@
 flask='export FLASK_APP=app.py FLASK_ENV=development' #Flask vars; TO DO in producton: remove 'FLASK_ENV=development'
 frun='python3 run.py'
 
+eval 'pkill -9 python3'
 eval $flask
 eval $frun
 
