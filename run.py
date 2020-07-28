23c658dd2d0e20b40d558e9fae6f8e765de166e6
uic82959 <andrei-alexandru.paciurca@continental-corporation.com>
2020-07-28 15:39:17 +0300

    modified:   run.py

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ from subprocess import Popen
 import os
 
 os.system("pkill -9 flask")
-flask = Popen(['/usr/bin/python3' ,'-m', 'app'], stdin=PIPE, stderr=PIPE, stdout=PIPE)
-
-print("Are you lost baby girl? All good! PID: "+str(os.getgid())+" from: "+str(os.getcwd())+"\n\n")
+#flask = Popen(['/usr/bin/python3' ,'-m', 'app'], stdin=PIPE, stderr=PIPE, stdout=PIPE)
+subprocess.call('python3 -m app', shell=True)
+print("\nAre you lost baby girl? All good!\n")
