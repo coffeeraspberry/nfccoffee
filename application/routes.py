4b21202f18c447bb6de47f0227e26afe08ea1bbb
uic82959 <andrei-alexandru.paciurca@continental-corporation.com>
2020-07-29 14:02:28 +0300

    modified:   application/routes.py

--- application/routes.py
+++ application/routes.py
@@ -8,7 +8,11 @@ def home():
 
 @app.route("/users", methods=['GET'])
 def users():
-    return json.dumps([u._asdict() for u in Users.query.all()], sort_keys=True) 
+    users = [u._asdict() for u in Users.query.all()]
+    for key in users:
+        users[key] = str(users[key])
+    return json.dumps([u._asdict() for u in Users.query.all()], sort_keys=True)
+    #return json.dumps([u._asdict() for u in Users.query.all()], sort_keys=True) 
 
 @app.route("/logs", methods=['GET'])
 def logs():
