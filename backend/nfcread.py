d6dd7c3069d7fd98861623e8b9df12faec7075bb
uic82959 <andrei-alexandru.paciurca@continental-corporation.com>
2020-07-30 13:13:10 +0300

    modified:   backend/nfcread.py

--- backend/nfcread.py
+++ backend/nfcread.py
@@ -1,5 +1,23 @@
 # demo purpose
 import time
+import subprocess
+import socket
+import board
+import busio
+import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
 
-time.sleep(30)
-print("\tHello World!\n")
+lcd_columns = 16
+lcd_rows = 2
+i2c = busio.I2C(board.SCL, board.SDA)
+lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows) 
+lcd.color = [100, 0, 0]
+
+hostname = socket.gethostname()
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+ip_address = s.getsockname()[0]
+s.close()
+
+lcd.clear()
+
+lcd.message = str(hostname)+"\n"+str(ip_address)
