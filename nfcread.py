0b77441bfc08b44b1f0d069f0341aeb352e9b3cf
uic82959 <andrei-alexandru.paciurca@continental-corporation.com>
2020-07-30 12:54:33 +0300

    deleted:    lcd.py 	modified:   nfcread.py

--- nfcread.py
+++ nfcread.py
@@ -1,5 +1,18 @@
 # demo purpose
 import time
+import socket
+import board
+import busio
+import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
 
-time.sleep(30)
-print("\tHello World!\n")
+lcd_columns = 16
+lcd_rows = 2
+i2c = busio.I2C(board.SCL, board.SDA)
+lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows) 
+lcd.color = [100, 0, 0]
+
+hostname = socket.gethostname()
+ip_address = socket.gethostbyname(hostname)
+lcd.clear()
+
+lcd.message = str(hostname)+"\n"+str(ip_address)
