494fd47189bf4625e3699c752ec58b9f765d66a5
uic82959 <andrei-alexandru.paciurca@continental-corporation.com>
2020-07-30 13:00:28 +0300

    modified:   nfcread.py

--- nfcread.py
+++ nfcread.py
@@ -1,5 +1,6 @@
 # demo purpose
 import time
+import subprocess
 import socket
 import board
 import busio
@@ -12,7 +13,8 @@ lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
 lcd.color = [100, 0, 0]
 
 hostname = socket.gethostname()
-ip_address = socket.gethostbyname(hostname)
+#ip_address = socket.gethostbyname(hostname)
+ip_address = subprocess.call("ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'", shell=True)
 lcd.clear()
 
 lcd.message = str(hostname)+"\n"+str(ip_address)
