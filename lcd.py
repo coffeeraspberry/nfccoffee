dbfad20366902ecdb65365698563a52ac90a362c
uic82959 <andrei-alexandru.paciurca@continental-corporation.com>
2020-07-30 12:39:30 +0300

    modified:   lcd.py

--- lcd.py
+++ lcd.py
@@ -15,4 +15,4 @@ i2c = busio.I2C(board.SCL, board.SDA)
 lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows) 
 
 lcd.color = [100, 0, 0]
-lcd.message = "{hostname}\n{ip_address}"
+lcd.message = str(hostname)+"\n"+str(ip_address)
