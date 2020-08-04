58b0a26e399218f5377694732fd7b421c9df8dc4
uic82959 <andrei-alexandru.paciurca@continental-corporation.com>
2020-08-04 13:54:06 +0300

    modified:   application/models.py 	deleted:    application/pi.db

--- application/models.py
+++ application/models.py
@@ -2,6 +2,7 @@
 from . import db
 from collections import OrderedDict
 from datetime import datetime
+from sqlalchemy.sql import func
 
 class DictSerializable(object):
     def _asdict(self):
@@ -19,11 +20,11 @@ class Users(db.Model, DictSerializable):
     userRelationship = db.relationship("Dates") # test
     UserName = db.Column(db.String(80), unique=True, nullable=False, default='user')
     Email = db.Column(db.String(120), unique=True, nullable=False, default='email')
-    Counter = db.Column(db.Integer, nullable=False, default=0)
-    LastAccess = db.Column(db.DateTime, nullable=True, default=datetime.utcnow())
+    Counter = db.Column(db.Integer, nullable=False, default=0, )
+    LastAccess = db.Column(db.DateTime, nullable=True, default=func.now())
 
 class Dates(db.Model, DictSerializable):
     __tablename__ = 'Dates'
     DateID = db.Column(db.Integer, primary_key=True, autoincrement=True)
     UserID = db.Column(db.Integer, db.ForeignKey('Users.UserID'))
-    DateLog = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
+    DateLog = db.Column(db.DateTime, nullable=False, default=func.now())
