77f56930e8808cc09f81c487e6645aaa35c72ac1
uic82959 <andrei-alexandru.paciurca@continental-corporation.com>
2020-08-12 15:33:51 +0300

    modified:   app.py 	modified:   run.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from application import app
-import subprocess
+import subprocess, threading
+
+def doSmth():
+     subprocess.call('python3 application/test.py',shell=True)
+     print("Am ajuns pana aici!")
 
 if __name__=='__main__':
+     #threading.Thread(doSmth).start()
      app.run(host='0.0.0.0', port=4321)
-     print("Am ajuns pana aici!")
+     threading.Thread(doSmth).start()
      
--- run.py
+++ run.py
@@ -3,5 +3,3 @@ import subprocess
 from time import sleep
 
 subprocess.call('python3 -m app &', shell=True)
-sleep(3)
-subprocess.call('python3 application/test.py',shell=True)
