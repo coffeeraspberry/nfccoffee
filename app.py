73d0777e5cae59c295f8662126da3b9897e5f405
uic82959 <andrei-alexandru.paciurca@continental-corporation.com>
2020-08-12 15:44:41 +0300

    modified:   app.py

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 from application import app
-import subprocess, threading
-
-def doSmth():
-     subprocess.call('python3 application/test.py',shell=True)
-     print("Am ajuns pana aici!")
 
 if __name__=='__main__':
-     #threading.Thread(doSmth).start()
      app.run(host='0.0.0.0', port=4321)
-     threading.Thread(doSmth).start()
+
      
