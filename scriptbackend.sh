6850b9fc81586447a8f2d07560dd89472d6060de
uic82959 <andrei-alexandru.paciurca@continental-corporation.com>
2020-08-13 11:32:26 +0300

    modified:   scriptbackend.sh

--- scriptbackend.sh
+++ scriptbackend.sh
@@ -2,4 +2,4 @@
 
 eval 'pkill -9 python3' #yep, pretty sure; not the most desirable thing to do but 'hey' it's working
 eval 'export FLASK_APP=app.py' # FLASK_ENV=development'
-eval 'python3 pyscripy.py'
+eval 'python3 pyscript.py'
