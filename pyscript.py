f7efc9434b552586be57d86d3894b3b3e45d600e
uic82959 <andrei-alexandru.paciurca@continental-corporation.com>
2020-08-13 10:16:30 +0300

    modified:   pyscript.py

--- pyscript.py
+++ pyscript.py
@@ -24,7 +24,7 @@ def incrementUserCounter(con,user):
 
 def addUserIfNotExists(con,uid):
     cursor = con.cursor()
-    cursor.execute('INSERT INTO Users(UserID) values('+str(uid.hex())+')')
+    cursor.execute('INSERT INTO Users(UserID) values(\''+str(uid.hex())+'\')')
     con.commit()
 
 lcd = character_lcd.Character_LCD_RGB_I2C(busio.I2C(board.SCL, board.SDA), 16, 2)
