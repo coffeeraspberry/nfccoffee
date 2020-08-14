32fdef631499d442c4f80175e1f834be1b75158c
uic82959 <andrei-alexandru.paciurca@continental-corporation.com>
2020-08-14 10:31:39 +0300

    modified:   pyscript.py

--- pyscript.py
+++ pyscript.py
@@ -9,9 +9,7 @@ from time import sleep
 import sqlite3
 import subprocess
 
-subprocess.call('python3 -m app &', shell=True)
 
-con = sqlite3.connect('application/pi.db')
 
 def getUser(con,UserID):
     cursor = con.cursor()
@@ -31,41 +29,48 @@ def addUserIfNotExists(con,uid):
     cursor.execute(insert)
     con.commit()
 
-lcd = character_lcd.Character_LCD_RGB_I2C(busio.I2C(board.SCL, board.SDA), 16, 2)
-lcd.color = [0, 0, 0]
+def mainf():
+    subprocess.call('python3 -m app &', shell=True)
 
-spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
-cs_pin = DigitalInOut(board.D8)
-pn532 = PN532_SPI(spi, cs_pin, debug=False)
-ic, ver, rev, support = pn532.firmware_version
-print("Found PN532 with firmware version: {0}.{1}".format(ver, rev))
-pn532.SAM_configuration()
+    con = sqlite3.connect('application/pi.db')
 
-hostname = socket.gethostname()
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("8.8.8.8", 80))
-ip_address = s.getsockname()[0]
-s.close()
+    lcd = character_lcd.Character_LCD_RGB_I2C(busio.I2C(board.SCL, board.SDA), 16, 2)
+    lcd.color = [0, 0, 0]
 
-while True:
-    # Check if a card is available to read
-    uid = pn532.read_passive_target(timeout=0.5)
-    print("UID: "+str(uid.hex()))
-    # Try again if no card is available.
-    if uid is None:
-        lcd.message = str(hostname)+"\n"+str(ip_address)
-    else:
-        our_user = getUser(con,str(uid.hex()))
-        if our_user:
-            lcd.message = "Found User:\n%s" %(our_user[0][2])
-            lcd.message = "Remove card!"
-            incrementUserCounter(con,our_user)
-            sleep(2)
+    spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+    cs_pin = DigitalInOut(board.D8)
+    pn532 = PN532_SPI(spi, cs_pin, debug=False)
+    ic, ver, rev, support = pn532.firmware_version
+    print("Found PN532 with firmware version: {0}.{1}".format(ver, rev))
+    pn532.SAM_configuration()
+
+    hostname = socket.gethostname()
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip_address = s.getsockname()[0]
+    s.close()
+
+    while True:
+        # Check if a card is available to read
+        uid = pn532.read_passive_target(timeout=0.5)
+        print("UID: "+str(uid.hex()))
+        # Try again if no card is available.
+        if uid is None:
+            lcd.message = str(hostname)+"\n"+str(ip_address)
         else:
-            addUserIfNotExists(con,str(uid.hex()))
-            lcd.message = "Remove card!"
-            sleep(2)
-            lcd.message = "Generic user added in DB\nVisit %s" %(str(ip_address))
-    sleep(1)
-    lcd.clear()     
+            our_user = getUser(con,str(uid.hex()))
+            if our_user:
+                lcd.message = "Found User:\n%s" %(our_user[0][2])
+                lcd.message = "Remove card!"
+                incrementUserCounter(con,our_user)
+                sleep(2)
+            else:
+                addUserIfNotExists(con,str(uid.hex()))
+                lcd.message = "Remove card!"
+                sleep(2)
+                lcd.message = "Generic user added in DB\nVisit %s" %(str(ip_address))
+        sleep(1)
+        lcd.clear()     
     
+if __name__=='__main__':
+     mainf()
