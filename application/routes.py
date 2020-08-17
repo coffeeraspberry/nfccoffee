289d26b45ab8954d4909f965155039f0219aa0ef
uic82959 <andrei-alexandru.paciurca@continental-corporation.com>
2020-08-17 15:18:45 +0300

    modified:   application/routes.py

--- application/routes.py
+++ application/routes.py
@@ -13,7 +13,7 @@ def findUser():
     #user = db.session.query(Users).filter_by(UserID=uid).firsr()
     #return json.dumps([usr._asdict() for usr in Users.query.filter_by(UserID=str(uid)).first()])    
     #temp = db.session.query(Users).filter_by(UserID='\'%s\'' %(uid)).first()
-    temp = Users.query.filter_by(UserID=2).first()
+    temp = Users.query.filter_by(UserID='fab0671a').first()
     print("Tmp e: %s" %(temp))
     return temp
 
