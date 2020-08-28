5862699955f45431dd05ba21f4c913b8c7b34c1f
uic82959 <andrei-alexandru.paciurca@continental-corporation.com>
2020-08-28 10:45:03 +0300

    modified:   logger.py

--- logger.py
+++ logger.py
@@ -3,7 +3,7 @@ import logging, logging.config
 #set log configuration
 logging.basicConfig(filename='coffee.log', filemode='a', format='TIMESTAMP: %(asctime)s PID: %(process)%d NAME: %(name)s - LEVEL: %(levelname)s - MESSAGE: %(message)s')
 log = logging.getLogger('CoffeCounter')
-log.setLevel(logging.DEBUG)
+log.setLevel(logging.INFO)
 file_handler = logging.FileHandler('coffee.log')
-file_handler.setLevel(logging.DEBUG)
+file_handler.setLevel(logging.INFO)
 log.addHandler(file_handler)
