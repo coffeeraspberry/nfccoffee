07ee7d803a0b88f802f9ef7ce90c07af962086a0
uic82959 <andrei-alexandru.paciurca@continental-corporation.com>
2020-08-31 11:46:00 +0300

    smth

--- application/routes.py
+++ application/routes.py
@@ -16,7 +16,7 @@ def require_api_token(func):
         # Check to see if it's in their session
         if 'api_session_token' not in session:
             # If it isn't return our access denied message (you can also return a redirect or render_template)
-            return Response("Access denied!"), 401
+            return make_response("Access denied!"), 401
         # Otherwise just send them where they wanted to go
         return func(*args, **kwargs)
 
