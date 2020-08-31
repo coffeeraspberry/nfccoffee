85603a217935e63b514f2fbdaae74dc7bf4f6b94
uic82959 <andrei-alexandru.paciurca@continental-corporation.com>
2020-08-31 11:08:21 +0300

    modified:   application/routes.py

--- application/routes.py
+++ application/routes.py
@@ -2,12 +2,25 @@
 from application import app
 from application.models import Users, Dates, Contact
 from . import db
-from flask import json, request, make_response
+from flask import json, request, make_response, session
 import os, signal, csv, subprocess, stream, logger
 from pyscript import interuptScan, scanBadge
 from time import sleep
 from logger import *
 import jwt, datetime
+from functools import wraps
+
+def require_api_token(func):
+    @wraps(func)
+    def check_token(*args, **kwargs):
+        # Check to see if it's in their session
+        if 'api_session_token' not in session:
+            # If it isn't return our access denied message (you can also return a redirect or render_template)
+            return Response("Access denied!"), 401
+        # Otherwise just send them where they wanted to go
+        return func(*args, **kwargs)
+
+    return check_token
 
 def findUser():
     log.info("findUser() function from application/routes.py  called")
@@ -31,11 +44,15 @@ def login():
     if data['Password'] == 'test':
         token = jwt.encode({'user' : data['Email'], 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
         print(token)
-        print(token.decode('UTF-8'))
+        session['api_session_token'] = token
         return json.dumps({'token' : token.decode('UTF-8')})
 
     return make_response('Could not verify',401,{'WWW-Authenticate' : 'Basic realm="Login Required"'})    
 
+@app.route("/admin/users", methods=['GET'])
+@require_api_token
+def users():
+    return json.dumps([u._asdict() for u in Users.query.all()], sort_keys=True)
 
 @app.route("/comment",methods=['POST'])
 def comment():
